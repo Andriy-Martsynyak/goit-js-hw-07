f89b4c019bf313ef40bb6af1b56f9f1d8f4bcbce
Andriy-Martsynyak <a.marcynak@ukr.net>
2022-09-21 20:13:43 +0300

    completed task #01

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector('.gallery');
+let closeModalVariable = null;
+
+const createGalleryTemplate = picturesList => {
+  return picturesList
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+  </a>
+</div>`;
+    })
+    .join('');
+};
+
+gallery.insertAdjacentHTML('afterbegin', createGalleryTemplate(galleryItems));
+
+const OnImgClick = e => {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const modal = basicLightbox.create(
+    `
+    <img src="${e.target.dataset.source}" width="800" height="600">`,
+    {
+      onClose: () => window.removeEventListener('keydown', escapeClose),
+    }
+  );
+
+  closeModalVariable = modal;
+  modal.show();
+
+  window.addEventListener('keydown', escapeClose);
+};
+
+gallery.addEventListener('click', OnImgClick);
+
+const escapeClose = e => {
+  console.log(e);
+
+  if (e.key === 'Escape') {
+    closeModalVariable.close();
+  }
+};
+
+gallery.addEventListener('keydown', escapeClose);
